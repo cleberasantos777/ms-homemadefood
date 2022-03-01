efbb0be6ea2e7041dd8d4d4d97257ffdcf2e149f
Cleber Santos <clebersantos7@yahoo.com.br>
2022-03-01 08:59:53 -0300

    hf-inventory created

---
+++ hf-inventory/src/main/java/com/gunnerapis/hfinventory/HfInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.gunnerapis.hfinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfInventoryApplication.class, args);
+	}
+
+}
---
+++ hf-inventory/src/test/java/com/gunnerapis/hfinventory/HfInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunnerapis.hfinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
